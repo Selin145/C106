93e0e3380f4bf8be518a124843b4f013fe1b1758
Selin145 <110917260+Selin145@users.noreply.github.com>
2024-05-20 09:48:04 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // https://teachablemachine.withgoogle.com/models/wXIYWAMu4/
+
+function startClassification()
+    {
+        navigator.mediaDevices.getUserMedia({audio: true});
+        classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/wXIYWAMu4/model.json', modelReady);
+
+    }
+    function modelReady(){
+        classifier.classify(gotResults);
+    }
